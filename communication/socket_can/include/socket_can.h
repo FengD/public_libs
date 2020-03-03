9be6b83c2b30329638dc9dd0d8e1ad5e686dab96
Feng DING <dingbobby@hotmail.com>
2020-03-03 15:23:01 +0800

    <feat>(socket_can): add socket can and change it as cpp style

---
+++ communication/socket_can/include/socket_can.h
@@ -0,0 +1,37 @@
+/* -*- mode: C++ -*-
+ *
+ *  Copyright (C) 2019 Feng DING, Hirain
+ *
+ *  License: Modified BSD Software License Agreement
+ *
+ */
+
+#ifndef _MODULE_COMMUNICATION_SOCKET_CAN_
+#define _MODULE_COMMUNICATION_SOCKET_CAN_
+
+#include <linux/can.h>
+#include <linux/can/raw.h>
+#include <linux/can/error.h>
+#include <sys/types.h>
+#include <stdint.h>
+
+namespace itd {
+namespace communication {
+class CanHandler;
+class CanConfig;
+class SocketCan {
+ private:
+  static int32_t can_socket_cfg(struct CanHandler *hdl, struct CanConfig *cfg);
+
+ public:
+  static int32_t can_open(struct CanHandler **hdl, struct CanConfig *cfg);
+  static int32_t can_close(struct CanHandler **hdl);
+  static int32_t can_read(struct CanHandler *hdl, struct can_frame *frame);
+  static int32_t can_write(struct CanHandler *hdl, const struct can_frame *frame);
+};
+
+}  // namespace itd
+}  // namespace communication
+
+
+#endif  //_MODULE_COMMUNICATION_SOCKET_CAN_
---
+++ communication/socket_can/src/socket_can.cpp
@@ -0,0 +1,148 @@
+/* -*- mode: C++ -*-
+ *
+ *  Copyright (C) 2019 Feng DING, Hirain
+ *
+ *  License: Modified BSD Software License Agreement
+ *
+ */
+
+
+#include <errno.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <signal.h>
+#include <sys/ioctl.h>
+#include <net/if.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include "socket_can.h"
+#include "hlog.h"
+#include "error_code.h"
+
+namespace itd {
+namespace communication {
+
+class CanHandler {
+ public:
+  int32_t fd;
+  int32_t ifindex;
+  bool fd_mode;
+};
+
+class CanConfig {
+ public:
+  char *ifname;
+  int32_t loopback;
+  size_t mtu;
+  int32_t is_filter_set;
+  struct can_filter *rx_filter;
+  size_t rx_filter_len;
+};
+
+int32_t SocketCan::can_socket_cfg(struct CanHandler *hdl, struct CanConfig *cfg) {
+  signal(SIGPIPE, SIG_IGN);
+  struct sockaddr_can addr;
+  struct ifreq ifr;
+
+  hdl->fd = socket(PF_CAN, SOCK_RAW, CAN_RAW);
+  if (hdl->fd < 0) {
+    LOGERROR("%s Error while opening socket. errno: %d\n", __func__, errno);
+    return ERR_IO;
+  }
+
+  addr.can_family  = AF_CAN;
+  strcpy(ifr.ifr_name, cfg->ifname);
+
+  if (ioctl(hdl->fd, SIOCGIFINDEX, &ifr)) {
+    LOGERROR("%s Error getting interface index. errno: %d\n",  __func__, errno);
+    return ERR_INTERFACE;
+  }
+  addr.can_ifindex = ifr.ifr_ifindex;
+
+  if (bind(hdl->fd, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
+    LOGERROR("%s Error in socket bind. errno: %d\n",  __func__, errno);
+    return ERR_BIND;
+  }
+
+   // whether receive own message 1 yes(default) 0 no
+  if (cfg->loopback >= 0 && cfg->loopback <= 1) {
+    setsockopt(hdl->fd, SOL_CAN_RAW, CAN_RAW_LOOPBACK, &cfg->loopback, sizeof(cfg->loopback));
+  }
+
+  if (cfg->is_filter_set){
+    if (cfg->rx_filter) {
+      setsockopt(hdl->fd, SOL_CAN_RAW, CAN_RAW_FILTER, cfg->rx_filter, cfg->rx_filter_len * sizeof(can_filter));
+    }
+  } else {
+     // if no need to receive, the line behind could save the space of receive queue in the kernal
+    setsockopt(hdl->fd, SOL_CAN_RAW, CAN_RAW_FILTER, NULL, 0);
+  }
+
+  can_err_mask_t err_mask = ( CAN_ERR_TRX | CAN_ERR_TX_TIMEOUT | CAN_ERR_CRTL | CAN_ERR_BUSOFF );
+  setsockopt(hdl->fd, SOL_CAN_RAW, CAN_RAW_ERR_FILTER, &err_mask, sizeof(err_mask));
+
+  return NO_ERROR;
+}
+
+int32_t SocketCan::can_open(struct CanHandler **hdl, struct CanConfig *cfg) {
+  *hdl = (struct CanHandler *) malloc(sizeof(struct CanHandler));
+  if (!*hdl) {
+    return ERR_IO;
+  }
+
+  memset(*hdl, 0, sizeof(struct CanHandler));
+
+  if (can_socket_cfg(*hdl, cfg) != 0) {
+    LOGERROR("%s Error in socket open. errno: %d\n",  __func__, errno);
+    free(*hdl);
+    *hdl = NULL;
+    return ERR_CAN_OPEN;
+  }
+
+  return NO_ERROR;
+}
+
+int32_t SocketCan::can_close(struct CanHandler **hdl) {
+  if (!hdl || !(*hdl)) {
+    LOGERROR("%s Error in socket close. errno: %d\n",  __func__, errno);
+    return ERR_IO;
+  }
+
+  if (close((*hdl)->fd) < 0) {
+    LOGERROR("%s Error in socket close. errno: %d\n",  __func__, errno);
+    return ERR_CAN_CLOSE;
+  }
+
+  free(*hdl);
+  *hdl = NULL;
+
+  return NO_ERROR;
+}
+
+int32_t SocketCan::can_read(struct CanHandler *hdl, struct can_frame *frame) {
+  ssize_t nbytes = -1;
+  nbytes = read(hdl->fd, frame, sizeof(struct can_frame));
+  if (nbytes == -1) {
+    LOGERROR("%s Error in read if nbytes == -1. errno: %d\n",  __func__, errno);
+    return ERR_CAN_READ;
+  } else if (nbytes == 0) {
+    LOGERROR("%s Error in read if nbytes == 0. errno: %d\n",  __func__, errno);
+    return ERR_CAN_READ;
+  }
+  return NO_ERROR;
+}
+
+int32_t SocketCan::can_write(struct CanHandler *hdl, const struct can_frame *frame) {
+  ssize_t nbytes = -1;
+  nbytes = write(hdl->fd, frame, sizeof(can_frame));
+  if (nbytes == -1) {
+    LOGERROR("%s Error in write nbytes == -1. errno: %d\n",  __func__, errno);
+    return ERR_CAN_WRITE;
+  }
+  usleep(100);
+  return NO_ERROR;
+}
+
+}  // namespace itd
+}  // namespace communication
