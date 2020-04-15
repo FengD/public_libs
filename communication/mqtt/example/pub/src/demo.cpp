d885a0dd58cc738790d21726ae2dc792ee39fa4e
Feng DING <dingbobby@hotmail.com>
2020-04-15 17:02:02 +0800

    <fix>(itd_ipd_libs/communication/mqtt): update the pub demo by using the mqtt_publisher

--- communication/mqtt/example/pub/src/demo.cpp
+++ communication/mqtt/example/pub/src/demo.cpp
@@ -2,22 +2,41 @@
 #include <stdlib.h>
 #include <mosquitto.h>
 #include <string.h>
-#include "hi_mqtt.h"
+#include "mqtt_publisher.h"
 
-#define HOST "localhost"
+#define HOST "192.168.8.210"
 #define PORT  1883
 #define KEEP_ALIVE 60
 
+void OnPublish(struct mosquitto *mosq, void *obj, int rc) {
+  (void)(obj);
+  (void)(rc);
+  (void)(mosq);
+  printf("Client publish.\n");
+}
+
+void OnLog(struct mosquitto *mosq, void *obj, int rc, const char *xx) {
+  (void)(obj);
+  (void)(rc);
+  (void)(mosq);
+  (void)(xx);
+  printf("Client log.\n");
+}
+
 int main() {
-  itd::communication::HiMqtt *client = new itd::communication::HiMqtt();
-  client->SetHost(HOST);
-  client->SetAliveTime(KEEP_ALIVE);
-  client->SetPort(PORT);
-  client->Init();
+  itd::communication::MqttPublisher *pub;
+  pub = new itd::communication::MqttPublisher(KEEP_ALIVE, HOST, PORT, "Test");
+  // if need
+  pub->SetOnPublish(OnPublish);
+  // if need
+  pub->SetOnLog(OnLog);
+  // if need username and password for mqtt server
+  // pub = new itd::communication::MqttPublisher(KEEP_ALIVE, HOST, PORT, "Test", "hirain", "hirain123");
+
   while(1) {
     printf("ds\n");
     char msg[] = "hahahahahaha";
-    client->Publish("test", msg, strlen(msg));
+    pub->Publish(msg, strlen(msg));
     sleep(1);
   }
   return 0;
