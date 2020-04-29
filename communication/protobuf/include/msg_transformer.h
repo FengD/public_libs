f95f64726618c11eb54914bca7c85bfe5c652008
Feng DING <dingbobby@hotmail.com>
2020-04-29 18:07:41 +0800

    <feat>(communication/mqtt): add msg transform

--- communication/protobuf/include/msg_transformer.h
+++ communication/protobuf/include/msg_transformer.h
@@ -15,10 +15,23 @@ namespace communication {
 class MsgTransformer {
  public:
   static void ToItdPointCloud(const pcl::PointCloud<pcl::PointXYZI> &cloud_in,
-                              itd::communication::protobuf::PointCloud &cloud_out);
+                                 itd::communication::protobuf::PointCloud &cloud_out);
 
-  // static void FromItdPointCloud(const itd::communication::protobuf::PointCloud &cloud_in,
-  //                               pcl::PointCloud<pcl::PointXYZI> &cloud_out);
+  static void ToItdPointCloudXYZ(const pcl::PointCloud<pcl::PointXYZI> &cloud_in,
+                                 itd::communication::protobuf::PointCloud &cloud_out);
+
+  static void ToItdPointCloudXYZI(const pcl::PointCloud<pcl::PointXYZI> &cloud_in,
+                                  itd::communication::protobuf::PointCloud &cloud_out);
+
+  static void FromItdPointCloud(const itd::communication::protobuf::PointCloud &cloud_in,
+                                pcl::PointCloud<pcl::PointXYZI> &cloud_out);
+
+  static void FromItdPointCloudXYZ(const itd::communication::protobuf::PointCloud &cloud_in,
+                                   pcl::PointCloud<pcl::PointXYZI> &cloud_out);
+
+  static void FromItdPointCloudXYZI(const itd::communication::protobuf::PointCloud &cloud_in,
+                                    pcl::PointCloud<pcl::PointXYZI> &cloud_out);
+
   static void ToItdFs(const float *fs_in, itd::communication::protobuf::Freespace &fs_out);
   //
   static void ToItdBridge(const float &k, const float &b, const int &flag,
--- communication/protobuf/src/msg_transformer.cpp
+++ communication/protobuf/src/msg_transformer.cpp
@@ -9,7 +9,18 @@ namespace itd {
 namespace communication {
 void MsgTransformer::ToItdPointCloud(const pcl::PointCloud<pcl::PointXYZI> &cloud_in,
                                      itd::communication::protobuf::PointCloud &cloud_out) {
+  ToItdPointCloudXYZI(cloud_in, cloud_out);
+  int cloud_size = cloud_in.size();
+  for (int i = 0; i < cloud_size; i++) {
+    cloud_out.add_time_offset(cloud_in.points[i].data[3]);
+    cloud_out.add_azimuth(cloud_in.points[i].data_c[1]);
+    cloud_out.add_ring(cloud_in.points[i].data_c[2]);
+    cloud_out.add_hor(cloud_in.points[i].data_c[3]);
+  }
+}
 
+void MsgTransformer::ToItdPointCloudXYZ(const pcl::PointCloud<pcl::PointXYZI> &cloud_in,
+                                        itd::communication::protobuf::PointCloud &cloud_out) {
   cloud_out.Clear();
   itd::communication::protobuf::Header *header = new itd::communication::protobuf::Header();
   header->set_seq(cloud_in.header.seq);
@@ -23,11 +34,72 @@ void MsgTransformer::ToItdPointCloud(const pcl::PointCloud<pcl::PointXYZI> &clou
     cloud_out.add_x(cloud_in.points[i].x);
     cloud_out.add_y(cloud_in.points[i].y);
     cloud_out.add_z(cloud_in.points[i].z);
+  }
+}
+
+void MsgTransformer::ToItdPointCloudXYZI(const pcl::PointCloud<pcl::PointXYZI> &cloud_in,
+                         itd::communication::protobuf::PointCloud &cloud_out) {
+  ToItdPointCloudXYZ(cloud_in, cloud_out);
+  int cloud_size = cloud_in.size();
+  for (int i = 0; i < cloud_size; i++) {
     cloud_out.add_intensity(cloud_in.points[i].intensity);
-    cloud_out.add_time_offset(cloud_in.points[i].data[3]);
-    cloud_out.add_azimuth(cloud_in.points[i].data_c[1]);
-    cloud_out.add_ring(cloud_in.points[i].data_c[2]);
-    cloud_out.add_hor(cloud_in.points[i].data_c[3]);
+  }
+}
+
+void MsgTransformer::FromItdPointCloud(const itd::communication::protobuf::PointCloud &cloud_in,
+                                       pcl::PointCloud<pcl::PointXYZI> &cloud_out) {
+  cloud_out.clear();
+  cloud_out.header.seq = cloud_in.header().seq();
+  cloud_out.header.stamp = cloud_in.header().stamp();
+  cloud_out.header.frame_id = cloud_in.header().frame_id();
+  int cloud_size = cloud_in.width();
+
+  for (int i = 0; i < cloud_size; i++) {
+    pcl::PointXYZI p;
+    p.x = cloud_in.x()[i];
+    p.y = cloud_in.y()[i];
+    p.z = cloud_in.z()[i];
+    p.intensity = cloud_in.intensity()[i];
+    p.data[3] = cloud_in.time_offset()[i];
+    p.data_c[1] = cloud_in.azimuth()[i];
+    p.data_c[2] = cloud_in.ring()[i];
+    p.data_c[3] = cloud_in.hor()[i];
+    cloud_out.push_back(std::move(p));
+  }
+}
+
+void MsgTransformer::FromItdPointCloudXYZ(const itd::communication::protobuf::PointCloud &cloud_in,
+                                          pcl::PointCloud<pcl::PointXYZI> &cloud_out) {
+  cloud_out.clear();
+  cloud_out.header.seq = cloud_in.header().seq();
+  cloud_out.header.stamp = cloud_in.header().stamp();
+  cloud_out.header.frame_id = cloud_in.header().frame_id();
+  int cloud_size = cloud_in.width();
+
+  for (int i = 0; i < cloud_size; i++) {
+    pcl::PointXYZI p;
+    p.x = cloud_in.x()[i];
+    p.y = cloud_in.y()[i];
+    p.z = cloud_in.z()[i];
+    cloud_out.push_back(std::move(p));
+  }
+}
+
+void MsgTransformer::FromItdPointCloudXYZI(const itd::communication::protobuf::PointCloud &cloud_in,
+                                           pcl::PointCloud<pcl::PointXYZI> &cloud_out) {
+  cloud_out.clear();
+  cloud_out.header.seq = cloud_in.header().seq();
+  cloud_out.header.stamp = cloud_in.header().stamp();
+  cloud_out.header.frame_id = cloud_in.header().frame_id();
+  int cloud_size = cloud_in.width();
+
+  for (int i = 0; i < cloud_size; i++) {
+    pcl::PointXYZI p;
+    p.x = cloud_in.x()[i];
+    p.y = cloud_in.y()[i];
+    p.z = cloud_in.z()[i];
+    p.intensity = cloud_in.intensity()[i];
+    cloud_out.push_back(std::move(p));
   }
 }
 
