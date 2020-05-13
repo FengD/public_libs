2d16e20b904d79edbaf11a9665bb0d5a1168e09b
Feng DING <dingbobby@hotmail.com>
2020-05-13 11:37:40 +0800

    <feat>(communication/json): add cpp json module used for parse string to json structure or reverce

---
+++ communication/json/BUILD.mk
@@ -0,0 +1,37 @@
+##############################################################################
+#
+# Freescale Confidential Proprietary
+#
+# Copyright (c) 2014 Freescale Semiconductor;
+# All Rights Reserved
+#
+##############################################################################
+#
+# THIS SOFTWARE IS PROVIDED BY FREESCALE "AS IS" AND ANY EXPRESSED OR
+# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+# IN NO EVENT SHALL FREESCALE OR ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+# INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+# STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+# IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+# THE POSSIBILITY OF SUCH DAMAGE.
+#
+##############################################################################
+
+SDK_ROOT := $(call path_relative_to,$(CURDIR),$(CURR_SDK_ROOT))
+
+ARM_LIB = libJson.a
+
+##############################################################################
+# libJson.a
+##############################################################################
+ARM_LIB_SRCS += json_reader.cpp
+ARM_LIB_SRCS += json_value.cpp
+ARM_LIB_SRCS += json_writer.cpp
+ARM_LIB_SRCS += json_valueiterator.inl
+
+ARM_INCS += -I$(SDK_ROOT)/include
+ARM_INCS += -I../include
---
+++ communication/json/README.md
@@ -0,0 +1,18 @@
+# Description
+
+Json Module
+
+# Change Log
+
+* Created by feng.ding @Hirain 20200408
+- 创建Json 使用json_cpp第三方库
+
+## Notice
+
+* 在未经master所有者授权情况下，禁止泄露源码到工程成员以外人员
+
+## 版本说明
+
+| Version | Update | Contractor | Description | Remark |
+| ------ | ------- | :----: | --------| --------- |
+| V1.0.0 | 2020/05/13 |  丁丰  | 创建并测试 | |
