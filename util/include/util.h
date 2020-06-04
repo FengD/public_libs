b11ee22054aa7f3fb3cc12997e401658d33a5c77
Feng DING <dingbobby@hotmail.com>
2020-06-04 13:35:00 +0800

    <fix>(util): correct cpplint and cppcheck error

--- util/include/util.h
+++ util/include/util.h
@@ -1,19 +1,21 @@
 // Copyright (C) 2020 Hirain Technologies
 // License: Modified BSD Software License Agreement
 // Author: Feng DING
-// Description:
+// Description: This module is an util module,
+//              which contains many useful functions
+// Date: 2019-01-01
+// Change Log:
 
-#ifndef _ITD_IPD_LIBS_UTIL_H_
-#define _ITD_IPD_LIBS_UTIL_H_
+#ifndef UTIL_INCLUDE_UTIL_H_
+#define UTIL_INCLUDE_UTIL_H_
 
 #include <sys/wait.h>
+#include <stdint.h>
 #include <string>
 #include <vector>
-#include <stdint.h>
 
 void calc_time_diff(const struct timespec *lo, const struct timespec *hi, struct timespec *diff);
-void printfTime(int32_t time);
 void split(const std::string &s, const std::string &seperator, std::vector<std::string> *result);
 void getAllFilesInFolder(const std::string& dir_in, std::vector<std::string> *files);
 
-#endif  // _ITD_IPD_LIBS_UTIL_H_
+#endif  // UTIL_INCLUDE_UTIL_H_
--- util/src/util.cpp
+++ util/src/util.cpp
@@ -1,36 +1,27 @@
 // Copyright (C) 2020 Hirain Technologies
 // License: Modified BSD Software License Agreement
 // Author: Feng DING
-// Description:
+// Description: This module is an util module,
+//              which contains many useful functions
+// Date: 2019-01-01
+// Change Log:
 
 #include "util.h"
 #include <string.h>
-#include <ctime>
 #include <time.h>
 #include <dirent.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <ctime>
 
 // time calculation
 void calc_time_diff(const struct timespec *lo, const struct timespec *hi, struct timespec *diff) {
-	diff->tv_sec = hi->tv_sec - lo->tv_sec;
-	diff->tv_nsec = hi->tv_nsec - lo->tv_nsec;
-	if (diff->tv_nsec < 0) {
-		diff->tv_sec--;
-		diff->tv_nsec += 1000000000;
-	}
-}
-
-void printfTime(int32_t time) {
-  time_t time_seconds(time);
-	struct tm* now_time = localtime(&time_seconds);
-	printf("%d-%d-%d %d:%d:%d\n",
-		now_time->tm_year + 1900,
-		now_time->tm_mon + 1,
-		now_time->tm_mday,
-		now_time->tm_hour,
-		now_time->tm_min,
-		now_time->tm_sec);
+  diff->tv_sec = hi->tv_sec - lo->tv_sec;
+  diff->tv_nsec = hi->tv_nsec - lo->tv_nsec;
+  if (diff->tv_nsec < 0) {
+    diff->tv_sec--;
+    diff->tv_nsec += 1000000000;
+  }
 }
 
 typedef std::string::size_type string_size;
@@ -39,12 +30,12 @@ void split(const std::string &s, const std::string &seperator, std::vector<std::
   result->clear();
 
   string_size i = 0;
-  while(i != s.size()) {
+  while (i != s.size()) {
     int32_t flag = 0;
-    while(i != s.size() && flag == 0) {
+    while (i != s.size() && flag == 0) {
       flag = 1;
-      for(string_size x = 0; x < seperator.size(); ++x) {
-        if(s[i] == seperator[x]) {
+      for (string_size x = 0; x < seperator.size(); ++x) {
+        if (s[i] == seperator[x]) {
           ++i;
           flag = 0;
           break;
@@ -54,19 +45,19 @@ void split(const std::string &s, const std::string &seperator, std::vector<std::
 
     flag = 0;
     string_size j = i;
-    while(j != s.size() && flag == 0) {
-      for(string_size x = 0; x < seperator.size(); ++x) {
-        if(s[j] == seperator[x]) {
+    while (j != s.size() && flag == 0) {
+      for (string_size x = 0; x < seperator.size(); ++x) {
+        if (s[j] == seperator[x]) {
           flag = 1;
           break;
         }
       }
 
-      if(flag == 0) {
+      if (flag == 0) {
         ++j;
       }
     }
-    if(i != j) {
+    if (i != j) {
       result->push_back(s.substr(i, j - i));
       i = j;
     }
@@ -87,15 +78,14 @@ void getAllFilesInFolder(const std::string& dir_in, std::vector<std::string> *fi
     std::exit(EXIT_FAILURE);
   }
   dirent* p = nullptr;
-  while( (p = readdir(open_dir)) != nullptr) {
+  while ((p = readdir(open_dir)) != nullptr) {
     struct stat st;
     if (p->d_name[0] != '.') {
       std::string name = dir_in + std::string("/") + std::string(p->d_name);
       stat(name.c_str(), &st);
       if (S_ISDIR(st.st_mode)) {
         getAllFilesInFolder(name, files);
-      }
-      else if (S_ISREG(st.st_mode)) {
+      } else if (S_ISREG(st.st_mode)) {
         files->push_back(name);
       }
     }
